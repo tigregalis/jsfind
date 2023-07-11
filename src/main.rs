4a3e3246429b0b9cdf740ac3765264890f196359
tigregalis <anak.harimau@gmail.com>
2023-07-11 19:54:04 +0800

    improve printing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use boa_engine::{js_string, Context, JsValue, Source};
 use colored::Colorize;
 use ignore::Walk;
+use pathdiff::diff_paths;
 use rayon::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args().skip(1);
     let filter = args.next().ok_or("filter expression not provided")?;
-
     let src = format!("(line => ({filter}))");
+
     // test the provided filter to make sure it's valid
     let mut context = Context::default();
     context
         .eval(Source::from_bytes(&src))?
         .as_callable()
-        .ok_or("expression evaluates to function")?
+        .ok_or("expression evaluates to function".red())?
         .call(
             &JsValue::undefined(),
             &[js_string!("An arbitrary string to test the filter expression").into()],
@@ -21,12 +22,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         )?;
 
     let current_dir = std::env::current_dir()?;
+    let current_dir = current_dir.as_path();
     let walker = Walk::new(current_dir).par_bridge();
     walker.for_each(|dent_result| {
         let dent = match dent_result {
             Ok(dent) => dent,
             Err(err) => {
-                eprintln!("{err}");
+                eprintln!("{err}", err = err.to_string().red());
                 return;
             }
         };
@@ -39,13 +41,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let path = dent.path();
 
-        let Some(file_name) = path.file_name().and_then(std::ffi::OsStr::to_str) else {
-            return;
-        };
         let file_contents = match std::fs::read_to_string(path) {
             Ok(s) => s,
             Err(err) => {
-                eprintln!("{err}");
+                eprintln!("{err}", err = err.to_string().red());
                 return;
             }
         };
@@ -69,15 +68,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 &mut context,
             ) {
                 Ok(v) => v,
-                Err(e) => {
+                Err(err) => {
                     let line_number = line_number + 1;
-                    eprintln!("error @ {file_name}:{line_number}: {e}");
+                    eprintln!(
+                        "error @ {file_name}:{line_number}: {err}",
+                        file_name = diff_paths(path, current_dir)
+                            .unwrap()
+                            .display()
+                            .to_string()
+                            .blue(),
+                        line_number = line_number.to_string().green(),
+                        err = err.to_string().red()
+                    );
                     continue;
                 }
             };
             if res.to_boolean() {
                 if !printed_file_name {
-                    println!("{}", file_name.blue());
+                    println!(
+                        "{}",
+                        diff_paths(path, current_dir)
+                            .unwrap()
+                            .display()
+                            .to_string()
+                            .blue()
+                    );
                     printed_file_name = true;
                 }
                 let line_number = format!("{n:>pad$}", n = line_number + 1);
